987a1290a44c380fe8e8183150681dd01b6ace04
Ryan Cook <rcook@redhat.com>
2023-12-15 15:00:49 -0500

    tuf

--- controllers/securesign_controller.go
+++ controllers/securesign_controller.go
@@ -386,11 +386,8 @@ func (r *SecuresignReconciler) createTrackedObjects(
 		return fmt.Errorf("could not ensure deployment: %w", err)
 	}
 	// TUF
-	// ensure the secret tuf-secrets exists before creating the deployment log but dont fail and move on
-	if err = r.Get(ctx, client.ObjectKey{Name: "tuf-secrets", Namespace: tun.Name}, &corev1.Secret{}); err == nil {
-		if _, err = r.ensureTufDeployment(ctx, instance, tun.Name, tstufsa.Name, "tuf"); err != nil {
-			return fmt.Errorf("could not ensure deployment: %w", err)
-		}
+	if _, err = r.ensureTufDeployment(ctx, instance, tun.Name, tstufsa.Name, "tuf"); err != nil {
+		return fmt.Errorf("could not ensure deployment: %w", err)
 	}
 	// Trusted Artifact Signer
 	if _, err = r.ensureTasDeployment(ctx, instance, tascs.Name, tascsa.Name, "tas-clients"); err != nil {
