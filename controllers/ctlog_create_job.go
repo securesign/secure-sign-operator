f56324762bef52e18f93f5215ded4fec77835544
Ryan Cook <rcook@redhat.com>
2023-12-14 22:14:38 -0500

    hack

--- controllers/ctlog_create_job.go
+++ controllers/ctlog_create_job.go
@@ -39,23 +39,24 @@ func (r *SecuresignReconciler) ensureCreateCTJob(ctx context.Context, m *rhtasv1
 					ServiceAccountName:           sA,
 					AutomountServiceAccountToken: &[]bool{true}[0],
 					RestartPolicy:                core.RestartPolicyNever,
-					InitContainers: []core.Container{
-						{
-							Name:  "wait-for-createtree-configmap",
-							Image: "registry.redhat.io/openshift4/ose-cli:latest",
-							Command: []string{
-								"sh",
-								"-c",
-								"until curl --fail --header \"Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)\" --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt --max-time 10 https://kubernetes.default.svc/api/v1/namespaces/" + namespace + "/configmaps/ctlog-config | grep '\"treeID\":'; do echo waiting for Configmap ctlog-config; sleep 5; done;",
-							},
-							Env: []core.EnvVar{
-								{
-									Name:  "NAMESPACE",
-									Value: namespace,
-								},
-							},
-						},
-					},
+					/*					InitContainers: []core.Container{
+											{
+												Name:  "wait-for-createtree-configmap",
+												Image: "registry.redhat.io/openshift4/ose-cli:latest",
+												Command: []string{
+													"sh",
+													"-c",
+													"until curl --fail --header \"Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)\" --cacert /var/run/secrets/kubernetes.io/serviceaccount/ca.crt --max-time 10 https://kubernetes.default.svc/api/v1/namespaces/" + namespace + "/configmaps/ctlog-config | grep '\"treeID\"':; do echo waiting for Configmap ctlog-config; sleep 5; done;",
+												},
+												Env: []core.EnvVar{
+													{
+														Name:  "NAMESPACE",
+														Value: namespace,
+													},
+												},
+											},
+										},
+					*/
 					Containers: []core.Container{
 						{
 							Name:  "trusted-artifact-signer-ctlog-createctconfig",
