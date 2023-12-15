8b43c00c025196fac540d87e4a9644a64e18c8f8
Ryan Cook <rcook@redhat.com>
2023-12-15 11:08:14 -0500

    print var right

--- controllers/configmap.go
+++ controllers/configmap.go
@@ -36,7 +36,7 @@ func (r *SecuresignReconciler) ensureConfigMap(ctx context.Context, m *rhtasv1al
 	// If the configMapName is rekor-sharding-config then replace the Data with the a multi-line empty string shared-config.yaml that creates without any quotes single or double
 	if configMapName == "rekor-sharding-config" {
 		configMap.Data = map[string]string{
-			"sharding-config.yaml": "|",
+			"sharding-config.yaml": `|`,
 		}
 	}
 
