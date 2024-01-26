85ce08441d4e4b3a5dfdec9cf836f3e8d8a569da
Jan Bouska <jbouska@redhat.com>
2024-01-26 12:19:00 +0100

    Stabilize CI (#94)

--- controllers/ctlog/utils/certificates.go
+++ controllers/ctlog/utils/certificates.go
@@ -2,10 +2,11 @@ package utils
 
 import (
 	"fmt"
-	fulcioclient "github.com/sigstore/fulcio/pkg/api"
-	"github.com/sigstore/sigstore/pkg/cryptoutils"
 	"net/url"
 	"time"
+
+	fulcioclient "github.com/sigstore/fulcio/pkg/api"
+	"github.com/sigstore/sigstore/pkg/cryptoutils"
 )
 
 type RootCertificate []byte
@@ -18,8 +19,13 @@ func GetFulcioRootCert(fulcioUrl string) (RootCertificate, error) {
 
 	var root *fulcioclient.RootResponse
 
-	client := fulcioclient.NewClient(u, fulcioclient.WithTimeout(time.Minute))
-	root, err = client.RootCert()
+	for i := 0; i < 10; i++ {
+		client := fulcioclient.NewClient(u)
+		root, err = client.RootCert()
+		if err != nil || root == nil {
+			time.Sleep(time.Duration(5) * time.Second)
+		}
+	}
 
 	if err != nil {
 		return nil, fmt.Errorf("failed to fetch Fulcio Root cert: %w", err)
