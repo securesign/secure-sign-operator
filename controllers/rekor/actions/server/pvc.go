10cf48f399962ea185ec85921d560b72300d9914
Ryan Cook <rcook@redhat.com>
2024-02-12 16:25:00 -0500

    set rekor size
    Signed-off-by: Ryan Cook <rcook@redhat.com>

--- controllers/rekor/actions/server/pvc.go
+++ controllers/rekor/actions/server/pvc.go
@@ -38,7 +38,7 @@ func (i createPvcAction) Handle(ctx context.Context, instance *rhtasv1alpha1.Rek
 	// PVC does not exist, create a new one
 	i.Logger.V(1).Info("Creating new PVC")
 	i.Recorder.Event(instance, v1.EventTypeNormal, "PersistentVolumeCreated", "New PersistentVolume created")
-	pvc := k8sutils.CreatePVC(instance.Namespace, fmt.Sprintf(PvcNameFormat, instance.Name), "5Gi", constants.LabelsFor(actions.ServerComponentName, actions.ServerDeploymentName, instance.Name))
+	pvc := k8sutils.CreatePVC(instance.Namespace, fmt.Sprintf(PvcNameFormat, instance.Name), instance.Spec.Pvc.Size, constants.LabelsFor(actions.ServerComponentName, actions.ServerDeploymentName, instance.Name))
 	if !instance.Spec.Pvc.Retain {
 		if err = controllerutil.SetControllerReference(instance, pvc, i.Client.Scheme()); err != nil {
 			return i.Failed(fmt.Errorf("could not set controller reference for PVC: %w", err))
