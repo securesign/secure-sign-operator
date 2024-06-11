94ff69978674f8d9e6efc01dff131ea981a4524b
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-06-11 13:25:52 +0000

    Update redis to 06382df

--- controllers/constants/images.go
+++ controllers/constants/images.go
@@ -10,7 +10,7 @@ var (
 
 	FulcioServerImage = "registry.redhat.io/rhtas/fulcio-rhel9@sha256:c4abc6342b39701d237ab3f0f25b75b677214b3ede00540b2488f524ad112179"
 
-	RekorRedisImage    = "registry.redhat.io/rhtas/trillian-redis-rhel9@sha256:5f0630c7aa29eeee28668f7ad451f129c9fb2feb86ec21b6b1b0b5cc42b44f4a"
+	RekorRedisImage    = "registry.redhat.io/rhtas/trillian-redis-rhel9@sha256:06382df99dfff9f002365a31eee0855f8032e351bc9583e42e489ab7623fe287"
 	RekorServerImage   = "registry.redhat.io/rhtas/rekor-server-rhel9@sha256:eed7af638b1587c61a76daef5df949bb37364023e5fa8a13255da02e2595f5ca"
 	RekorSearchUiImage = "registry.redhat.io/rhtas/rekor-search-ui-rhel9@sha256:03fa0d23079aa4146d6d7b3f4edaa302e383e7d0a6c15cbf73a58179f1d07e02"
 	BackfillRedisImage = "registry.redhat.io/rhtas/rekor-backfill-redis-rhel9@sha256:5c7460ab3cd13b2ecf2b979f5061cb384174d6714b7630879e53d063e4cb69d2"
