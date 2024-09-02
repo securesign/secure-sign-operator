ccronca/securesign_secure-sign-operator

[SECURESIGN-1238] Align with new TUF server

internal/controller/tuf/actions/constants.go
@@ -6,4 +6,7 @@ const (
 	RBACName       = "tuf"
 	PortName       = "http"
 	Port           = 8080
+	InitJobName    = "tuf-repository-init"
+
+	RepositoryCondition = "tufRepository"
 )

internal/controller/tuf/utils/tuf_init_job.go
@@ -1,0 +1,81 @@
+package utils
+
+import (
+	"github.com/operator-framework/operator-lib/proxy"
+	"github.com/securesign/operator/api/v1alpha1"
+	"github.com/securesign/operator/internal/controller/constants"
+	v1 "k8s.io/api/batch/v1"
+	core "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+)
+
+func CreateTufInitJob(instance *v1alpha1.Tuf, name string, sa string, labels map[string]string) *v1.Job {
+	env := []core.EnvVar{
+		{
+			Name:  "NAMESPACE",
+			Value: instance.Namespace,
+		},
+	}
+	env = append(env, proxy.ReadProxyVarsFromEnv()...)
+	job := &v1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: instance.Namespace,
+			Labels:    labels,
+		},
+		Spec: v1.JobSpec{
+			Parallelism: ptr.To[int32](1),
+			Completions: ptr.To[int32](1),
+
+			BackoffLimit: ptr.To[int32](3),
+			Template: core.PodTemplateSpec{
+				Spec: core.PodSpec{
+					// this should be enough to create repository
+					ActiveDeadlineSeconds: ptr.To[int64](60),
+					ServiceAccountName:    sa,
+					RestartPolicy:         core.RestartPolicyOnFailure,
+					Volumes: []core.Volume{
+						{
+							Name: "tuf-secrets",
+							VolumeSource: core.VolumeSource{
+								Projected: secretsVolumeProjection(instance.Status.Keys),
+							},
+						},
+						{
+							Name: "repository",
+							VolumeSource: core.VolumeSource{
+								PersistentVolumeClaim: &core.PersistentVolumeClaimVolumeSource{
+									ClaimName: instance.Status.PvcName,
+								},
+							},
+						},
+					},
+					Containers: []core.Container{
+						{
+							Name:  "tuf-init",
+							Image: constants.TufImage,
+							Env:   env,
+							Args: []string{
+								"-mode", "init-no-overwrite",
+								"-target-dir", "/var/run/target",
+							},
+							VolumeMounts: []core.VolumeMount{
+								{
+									Name:      "tuf-secrets",
+									MountPath: "/var/run/tuf-secrets",
+								},
+								{
+									Name:      "repository",
+									MountPath: "/var/run/target",
+									ReadOnly:  false,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return job
+}

securesign/secure-sign-operator

internal/controller/tuf/actions/constants.go
@@ -6,4 +6,7 @@ const (
 	RBACName       = "tuf"
 	PortName       = "http"
 	Port           = 8080
+	InitJobName    = "tuf-repository-init"
+
+	RepositoryCondition = "repository"
 )

internal/controller/tuf/utils/tuf_init_job.go
@@ -1,0 +1,78 @@
+package utils
+
+import (
+	"github.com/operator-framework/operator-lib/proxy"
+	"github.com/securesign/operator/api/v1alpha1"
+	"github.com/securesign/operator/internal/controller/constants"
+	v1 "k8s.io/api/batch/v1"
+	core "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
+)
+
+func CreateTufInitJob(instance *v1alpha1.Tuf, name string, sa string, labels map[string]string) *v1.Job {
+	env := []core.EnvVar{
+		{
+			Name:  "NAMESPACE",
+			Value: instance.Namespace,
+		},
+	}
+	env = append(env, proxy.ReadProxyVarsFromEnv()...)
+	job := &v1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: instance.Namespace,
+			Labels:    labels,
+		},
+		Spec: v1.JobSpec{
+			Parallelism: ptr.To[int32](1),
+			Completions: ptr.To[int32](1),
+			Template: core.PodTemplateSpec{
+				Spec: core.PodSpec{
+					ServiceAccountName: sa,
+					RestartPolicy:      core.RestartPolicyNever,
+					Volumes: []core.Volume{
+						{
+							Name: "tuf-secrets",
+							VolumeSource: core.VolumeSource{
+								Projected: SecretsVolumeProjection(instance.Status.Keys),
+							},
+						},
+						{
+							Name: "repository",
+							VolumeSource: core.VolumeSource{
+								PersistentVolumeClaim: &core.PersistentVolumeClaimVolumeSource{
+									ClaimName: instance.Status.PvcName,
+								},
+							},
+						},
+					},
+					Containers: []core.Container{
+						{
+							Name:  "tuf-init",
+							Image: constants.TufImage,
+							Env:   env,
+							Args: []string{
+								"-mode", "init-no-overwrite",
+								"-target-dir", "/var/run/target",
+								"-keyssecret", instance.Spec.RootKeySecretRef,
+							},
+							VolumeMounts: []core.VolumeMount{
+								{
+									Name:      "tuf-secrets",
+									MountPath: "/var/run/tuf-secrets",
+								},
+								{
+									Name:      "repository",
+									MountPath: "/var/run/target",
+									ReadOnly:  false,
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+	return job
+}