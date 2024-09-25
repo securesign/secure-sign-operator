c99a11c284d77e680af7029633f5ad4a0350f884
Firas Ghanmi <fghanmi@redhat.com>
2024-09-25 16:01:47 +0200

    RC3: update images

--- internal/controller/constants/images.go
+++ internal/controller/constants/images.go
@@ -1,27 +1,27 @@
 package constants
 
 var (
-	TrillianLogSignerImage = "registry.redhat.io/rhtas/trillian-logsigner-rhel9@sha256:b83d806de7d9653d4ce4cf2c1db7b5f8aa607f3888a99c4924477b5cfb48c930"
-	TrillianServerImage    = "registry.redhat.io/rhtas/trillian-logserver-rhel9@sha256:3d017de5adf2ab58f5a87dcad5ccd38a2a40003834ef09d3bc17d8946387fa05"
-	TrillianDbImage        = "registry.redhat.io/rhtas/trillian-database-rhel9@sha256:2f20f41d6646d3b3fe816663491a4fa86b362d1c42b8bd1968a6be301eeb11fe"
+	TrillianLogSignerImage = "registry.redhat.io/rhtas/trillian-logsigner-rhel9@sha256:d438373fc3b53b0e318098fc5587e265c0603d7ab3e11fb9819e747a5e2f18a6"
+	TrillianServerImage    = "registry.redhat.io/rhtas/trillian-logserver-rhel9@sha256:ccf05883ab992fcf6c3058e6d897c4ce85c13aad54a96499ddeff4a5f056a834"
+	TrillianDbImage        = "registry.redhat.io/rhtas/trillian-database-rhel9@sha256:ffa1f4f94eb91169c39e5477c0d10ed3020d5fb792651f7464c6465c9d8f558c"
 
 	// TODO: remove and check the DB pod status
 	TrillianNetcatImage = "registry.redhat.io/openshift4/ose-tools-rhel8@sha256:486b4d2dd0d10c5ef0212714c94334e04fe8a3d36cf619881986201a50f123c7"
 
-	FulcioServerImage = "registry.redhat.io/rhtas/fulcio-rhel9@sha256:02dc2af135b4eaa16deec597187fc9c4eb1d7e395631d0566df80eb3e0aaa84e"
+	FulcioServerImage = "registry.redhat.io/rhtas/fulcio-rhel9@sha256:a1f0c021e9e291394eec61fdd07068466d59a58d61c60e27300a9bae49acb648"
 
-	RekorRedisImage    = "registry.redhat.io/rhtas/trillian-redis-rhel9@sha256:16ad1b2a0fc40792e26b3b84868315064469250b24321d5ffb7980c0e7b029da"
-	RekorServerImage   = "registry.redhat.io/rhtas/rekor-server-rhel9@sha256:7f7ede4c0a51d3f8c459cc86bcd33c8858992764f910d4c882c55bf7bcbeb91f"
-	RekorSearchUiImage = "registry.redhat.io/rhtas/rekor-search-ui-rhel9@sha256:9973ce7c5ceed4a6f5b45c69a22ba98f5c6ad324e212ba882415b85488528fc1"
-	BackfillRedisImage = "registry.redhat.io/rhtas/rekor-backfill-redis-rhel9@sha256:02d409438a038007f831abaf9eac3cd86f203fbb6b6dece4d0f70dc1b52cd79c"
+	RekorRedisImage    = "registry.redhat.io/rhtas/trillian-redis-rhel9@sha256:c89a4891a7054b5cd3774358406ed87eba0ce547dbfd958fb61b0de24140a963"
+	RekorServerImage   = "registry.redhat.io/rhtas/rekor-server-rhel9@sha256:ba9c0c19aaf682f5cb31aa810da9f7be762a965a2840841880d8a74dac2513f6"
+	RekorSearchUiImage = "registry.redhat.io/rhtas/rekor-search-ui-rhel9@sha256:e01cfb64b011a14a243701a992b7fc3dbc9d7a181bea5cebcfc28593613f7950"
+	BackfillRedisImage = "registry.redhat.io/rhtas/rekor-backfill-redis-rhel9@sha256:b94f07e8ca990957614d5a942c65400b117663b4b2bfaa4af152fda9cb09130b"
 
-	TufImage = "registry.redhat.io/rhtas/tuffer-rhel9@sha256:1beb250dfc24a0c094799afda075989cf6f7eb1212d655571fc9054f74961f89"
+	TufImage = "registry.redhat.io/rhtas/tuffer-rhel9@sha256:410c5b4f6fd4c50adb8c62a59dadfc9dce9de7a0023b58c9c337f297d7aad0f2"
 
-	CTLogImage = "registry.redhat.io/rhtas/certificate-transparency-rhel9@sha256:6fef78c77e6b2a926d7535d46d86e7bbda3e30ad9d5d6653bcb96698b56594fc"
+	CTLogImage = "registry.redhat.io/rhtas/certificate-transparency-rhel9@sha256:053ce379d83675818ddb6f5869bc986d1e210ef8607d081b6b05eb4d4255aa82"
 
 	HttpServerImage = "registry.access.redhat.com/ubi9/httpd-24@sha256:7874b82335a80269dcf99e5983c2330876f5fe8bdc33dc6aa4374958a2ffaaee"
 
 	SegmentBackupImage      = "registry.redhat.io/rhtas/segment-reporting-rhel9@sha256:c7fa18f6dec1fdd308d5a6ed74f5f6bf2bd30d6759d7d2464875b6e80f269fb2"
 	TimestampAuthorityImage = "registry.redhat.io/rhtas/timestamp-authority-rhel9@sha256:d957041e1f10faf087333b9f1d39b2bb4b26edd37a812192e67771c423950def"
-	ClientServerImage       = "registry.redhat.io/rhtas/client-server-rhel9@sha256:ebb18fe7236c4c234ce8c634aea0932e4a2b50333ae3809cb34746c0fad0241c"
+	ClientServerImage       = "registry.redhat.io/rhtas/client-server-rhel9@sha256:eea8539812775b24be97fa2d62606be0ab3403581c1a4b9ae115e25c2dc4a4c5"
 )
