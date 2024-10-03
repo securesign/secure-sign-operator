a8ece733f7277e62c84fdb7a0fe600440c7bf1cc
JasonPowr <japower@redhat.com>
2024-10-03 10:30:39 +0100

    update rekor and cli images

--- internal/controller/constants/images.go
+++ internal/controller/constants/images.go
@@ -11,9 +11,9 @@ var (
 	FulcioServerImage = "registry.redhat.io/rhtas/fulcio-rhel9@sha256:a1f0c021e9e291394eec61fdd07068466d59a58d61c60e27300a9bae49acb648"
 
 	RekorRedisImage    = "registry.redhat.io/rhtas/trillian-redis-rhel9@sha256:c89a4891a7054b5cd3774358406ed87eba0ce547dbfd958fb61b0de24140a963"
-	RekorServerImage   = "registry.redhat.io/rhtas/rekor-server-rhel9@sha256:ba9c0c19aaf682f5cb31aa810da9f7be762a965a2840841880d8a74dac2513f6"
+	RekorServerImage   = "registry.redhat.io/rhtas/rekor-server-rhel9@sha256:45608d663642dce69e3d91101913ba3cc70c2a95d0ce9a23c9116293cd7bc246"
 	RekorSearchUiImage = "registry.redhat.io/rhtas/rekor-search-ui-rhel9@sha256:e01cfb64b011a14a243701a992b7fc3dbc9d7a181bea5cebcfc28593613f7950"
-	BackfillRedisImage = "registry.redhat.io/rhtas/rekor-backfill-redis-rhel9@sha256:b94f07e8ca990957614d5a942c65400b117663b4b2bfaa4af152fda9cb09130b"
+	BackfillRedisImage = "registry.redhat.io/rhtas/rekor-backfill-redis-rhel9@sha256:c454ee91ffc8a1d028d44b999370d98eef751cbfb2b211622d945f51b8afdb84"
 
 	TufImage = "registry.redhat.io/rhtas/tuffer-rhel9@sha256:410c5b4f6fd4c50adb8c62a59dadfc9dce9de7a0023b58c9c337f297d7aad0f2"
 
@@ -23,5 +23,5 @@ var (
 
 	SegmentBackupImage      = "registry.redhat.io/rhtas/segment-reporting-rhel9@sha256:c7fa18f6dec1fdd308d5a6ed74f5f6bf2bd30d6759d7d2464875b6e80f269fb2"
 	TimestampAuthorityImage = "registry.redhat.io/rhtas/timestamp-authority-rhel9@sha256:d957041e1f10faf087333b9f1d39b2bb4b26edd37a812192e67771c423950def"
-	ClientServerImage       = "registry.redhat.io/rhtas/client-server-rhel9@sha256:c234844ce59fd49960fa84c417872eefaa836559b88cead4ada1b1ffb56c8c21"
+	ClientServerImage       = "registry.redhat.io/rhtas/client-server-rhel9@sha256:e81f157c97a55cc572bb3a9abcc7d15ccf840b14620ca653b550a0ea9e83da70"
 )
