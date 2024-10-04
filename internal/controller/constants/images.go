f37af94d0ca3d3a2158ba5140d13c24605a31145
Firas Ghanmi <158172821+fghanmi@users.noreply.github.com>
2024-10-04 16:18:29 +0200

    RC5: update images (#664)

--- internal/controller/constants/images.go
+++ internal/controller/constants/images.go
@@ -12,16 +12,16 @@ var (
 
 	RekorRedisImage    = "registry.redhat.io/rhtas/trillian-redis-rhel9@sha256:c89a4891a7054b5cd3774358406ed87eba0ce547dbfd958fb61b0de24140a963"
 	RekorServerImage   = "registry.redhat.io/rhtas/rekor-server-rhel9@sha256:45608d663642dce69e3d91101913ba3cc70c2a95d0ce9a23c9116293cd7bc246"
-	RekorSearchUiImage = "registry.redhat.io/rhtas/rekor-search-ui-rhel9@sha256:e01cfb64b011a14a243701a992b7fc3dbc9d7a181bea5cebcfc28593613f7950"
+	RekorSearchUiImage = "registry.redhat.io/rhtas/rekor-search-ui-rhel9@sha256:e9233bc0f5d1d441385253771ea4896e16446f08a594553c3d3b182c6e9bb96d"
 	BackfillRedisImage = "registry.redhat.io/rhtas/rekor-backfill-redis-rhel9@sha256:c454ee91ffc8a1d028d44b999370d98eef751cbfb2b211622d945f51b8afdb84"
 
-	TufImage = "registry.redhat.io/rhtas/tuffer-rhel9@sha256:410c5b4f6fd4c50adb8c62a59dadfc9dce9de7a0023b58c9c337f297d7aad0f2"
+	TufImage = "registry.redhat.io/rhtas/tuffer-rhel9@sha256:a14a78c2d634e2ba7cdf57f31901ba67c2e905ec37d129be6c22d8f233336a35"
 
 	CTLogImage = "registry.redhat.io/rhtas/certificate-transparency-rhel9@sha256:053ce379d83675818ddb6f5869bc986d1e210ef8607d081b6b05eb4d4255aa82"
 
 	HttpServerImage = "registry.access.redhat.com/ubi9/httpd-24@sha256:7874b82335a80269dcf99e5983c2330876f5fe8bdc33dc6aa4374958a2ffaaee"
 
 	SegmentBackupImage      = "registry.redhat.io/rhtas/segment-reporting-rhel9@sha256:c7fa18f6dec1fdd308d5a6ed74f5f6bf2bd30d6759d7d2464875b6e80f269fb2"
-	TimestampAuthorityImage = "registry.redhat.io/rhtas/timestamp-authority-rhel9@sha256:d957041e1f10faf087333b9f1d39b2bb4b26edd37a812192e67771c423950def"
+	TimestampAuthorityImage = "registry.redhat.io/rhtas/timestamp-authority-rhel9@sha256:413b27ba32209d9432e1894ac0a7c095332ddc52683c6e444fc4329644751fed"
 	ClientServerImage       = "registry.redhat.io/rhtas/client-server-rhel9@sha256:e81f157c97a55cc572bb3a9abcc7d15ccf840b14620ca653b550a0ea9e83da70"
 )
