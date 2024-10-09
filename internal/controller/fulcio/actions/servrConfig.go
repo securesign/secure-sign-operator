138a5fb2d6bc6111f937070166dacea6805164f3
Jan Bouska <jbouska@redhat.com>
2024-10-09 18:11:37 +0200

    [SECURESIGN-1456] Ensure that Fulcio component does not create duplicated resources

--- internal/controller/fulcio/actions/servrConfig.go
+++ internal/controller/fulcio/actions/servrConfig.go
@@ -3,8 +3,11 @@ package actions
 import (
 	"context"
 	"fmt"
+	"reflect"
 
 	"gopkg.in/yaml.v2"
+	labels2 "k8s.io/apimachinery/pkg/labels"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	rhtasv1alpha1 "github.com/securesign/operator/api/v1alpha1"
 	"github.com/securesign/operator/internal/controller/common/action"
@@ -14,6 +17,11 @@ import (
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+)
+
+const (
+	configResourceLabel = "server-config"
+	serverConfigName    = "config.yaml"
 )
 
 func NewServerConfigAction() action.Action[*rhtasv1alpha1.Fulcio] {
@@ -35,24 +43,15 @@ type FulcioMapConfig struct {
 
 func (i serverConfig) CanHandle(ctx context.Context, instance *rhtasv1alpha1.Fulcio) bool {
 	c := meta.FindStatusCondition(instance.Status.Conditions, constants.Ready)
-	if c.Reason != constants.Creating && c.Reason != constants.Ready {
+	switch {
+	case c == nil:
 		return false
-	}
-
-	if instance.Status.ServerConfigRef == nil {
+	case c.Reason != constants.Creating && c.Reason != constants.Ready:
+		return false
+	default:
 		return true
 	}
-	existing, err := kubernetes.GetConfigMap(ctx, i.Client, instance.Namespace, instance.Status.ServerConfigRef.Name)
-	if err != nil {
-		i.Logger.Error(err, "Cant load existing configuration")
-		return false
-	}
-	expected, err := yaml.Marshal(ConvertToFulcioMapConfig(instance.Spec.Config))
-	if err != nil {
-		i.Logger.Error(err, "Cant parse expected configuration")
-		return false
-	}
-	return existing.Data["config.yaml"] != string(expected)
+
 }
 
 func ConvertToFulcioMapConfig(fulcioConfig rhtasv1alpha1.FulcioConfig) *FulcioMapConfig {
@@ -79,38 +78,67 @@ func (i serverConfig) Handle(ctx context.Context, instance *rhtasv1alpha1.Fulcio
 		err error
 	)
 	labels := constants.LabelsFor(ComponentName, DeploymentName, instance.Name)
+	labels[constants.LabelResource] = configResourceLabel
 
 	config, err := yaml.Marshal(ConvertToFulcioMapConfig(instance.Spec.Config))
 	if err != nil {
 		return i.FailedWithStatusUpdate(ctx, err, instance)
 	}
-	expected := kubernetes.CreateImmutableConfigmap(fmt.Sprintf("fulcio-config-%s", instance.Name), instance.Namespace, labels, map[string]string{
-		"config.yaml": string(config),
-	})
-	if err = controllerutil.SetControllerReference(instance, expected, i.Client.Scheme()); err != nil {
-		return i.Failed(fmt.Errorf("could not set controller reference for ConfigMap: %w", err))
-	}
 
-	// invalidate config
+	// verify existing config
 	if instance.Status.ServerConfigRef != nil {
-		if err = i.Client.Delete(ctx, &v1.ConfigMap{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:      instance.Status.ServerConfigRef.Name,
-				Namespace: instance.Namespace,
-			},
-		}); err != nil {
-			return i.Failed(err)
+		cfg, err := kubernetes.GetConfigMap(ctx, i.Client, instance.Namespace, instance.Status.ServerConfigRef.Name)
+		if client.IgnoreNotFound(err) != nil {
+			return i.Failed(fmt.Errorf("FulcioConfig: %w", err))
 		}
-		instance.Status.ServerConfigRef = nil
+		if cfg != nil {
+			if reflect.DeepEqual(cfg.Data[serverConfigName], string(config)) {
+				return i.Continue()
+			} else {
+				i.Logger.Info("Remove invalid ConfigMap with fulcio-server configuration", "Name", cfg.Name)
+				_ = i.Client.Delete(ctx, cfg)
+			}
+		}
+	}
+	// invalidate
+	instance.Status.ServerConfigRef = nil
+
+	// try to discover existing config
+	partialConfigs, err := kubernetes.ListConfigMaps(ctx, i.Client, instance.Namespace, labels2.SelectorFromSet(labels).String())
+	if err != nil {
+		i.Logger.Error(err, "problem with finding configmap", "namespace", instance.Namespace)
+	}
+	for _, partialConfig := range partialConfigs.Items {
+		cm, err := kubernetes.GetConfigMap(ctx, i.Client, partialConfig.Namespace, partialConfig.Name)
+		if err != nil {
+			return i.Failed(fmt.Errorf("can't load configMap data %w", err))
+		}
+		if reflect.DeepEqual(cm.Data[serverConfigName], string(config)) && instance.Status.ServerConfigRef == nil {
+			i.Recorder.Eventf(instance, v1.EventTypeNormal, "FulcioConfigDiscovered", "Existing ConfigMap with fulcio configuration discovered: %s", cm.Name)
+			instance.Status.ServerConfigRef = &rhtasv1alpha1.LocalObjectReference{Name: cm.Name}
+			meta.SetStatusCondition(&instance.Status.Conditions, metav1.Condition{
+				Type:    constants.Ready,
+				Status:  metav1.ConditionFalse,
+				Reason:  constants.Creating,
+				Message: "Server config discovered"})
+		} else {
+			i.Logger.Info("Remove invalid ConfigMap with rekor-server configuration", "Name", cm.Name)
+			_ = i.Client.Delete(ctx, cm)
+		}
+	}
+	if instance.Status.ServerConfigRef != nil {
+		return i.StatusUpdate(ctx, instance)
 	}
 
-	if err = i.Client.Create(ctx, expected); err != nil {
-		meta.SetStatusCondition(&instance.Status.Conditions, metav1.Condition{
-			Type:    CertCondition,
-			Status:  metav1.ConditionFalse,
-			Reason:  constants.Failure,
-			Message: err.Error(),
-		})
+	// create new config
+	newConfig := kubernetes.CreateImmutableConfigmap("fulcio-config-", instance.Namespace, labels, map[string]string{
+		serverConfigName: string(config)})
+	if err = controllerutil.SetControllerReference(instance, newConfig, i.Client.Scheme()); err != nil {
+		return i.Failed(fmt.Errorf("FulcioConfig: could not set controller reference for ConfigMap: %w", err))
+	}
+
+	_, err = i.Ensure(ctx, newConfig)
+	if err != nil {
 		meta.SetStatusCondition(&instance.Status.Conditions, metav1.Condition{
 			Type:    constants.Ready,
 			Status:  metav1.ConditionFalse,
@@ -119,12 +147,15 @@ func (i serverConfig) Handle(ctx context.Context, instance *rhtasv1alpha1.Fulcio
 		})
 		return i.FailedWithStatusUpdate(ctx, err, instance)
 	}
-
-	instance.Status.ServerConfigRef = &rhtasv1alpha1.LocalObjectReference{Name: expected.Name}
-
 	i.Recorder.Event(instance, v1.EventTypeNormal, "FulcioConfigUpdated", "Fulcio config updated")
-	meta.SetStatusCondition(&instance.Status.Conditions, metav1.Condition{Type: constants.Ready,
-		Status: metav1.ConditionFalse, Reason: constants.Creating, Message: "Server config created"})
-	return i.StatusUpdate(ctx, instance)
+	instance.Status.ServerConfigRef = &rhtasv1alpha1.LocalObjectReference{Name: newConfig.Name}
 
+	meta.SetStatusCondition(&instance.Status.Conditions,
+		metav1.Condition{
+			Type:    constants.Ready,
+			Status:  metav1.ConditionFalse,
+			Reason:  constants.Creating,
+			Message: "Server config created"},
+	)
+	return i.StatusUpdate(ctx, instance)
 }
