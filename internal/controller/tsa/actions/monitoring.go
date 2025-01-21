af14170f74ca8bca14bc8c01059a993021096ec1
Jan Bouska <jbouska@redhat.com>
2025-01-21 14:48:56 +0100

    [SECURESIGN-1393] Migrate monitors

--- internal/controller/tsa/actions/monitoring.go
+++ internal/controller/tsa/actions/monitoring.go
@@ -15,7 +15,6 @@ import (
 	v1 "k8s.io/api/rbac/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
@@ -82,33 +81,22 @@ func (i monitoringAction) Handle(ctx context.Context, instance *rhtasv1alpha1.Ti
 		return i.Error(ctx, reconcile.TerminalError(fmt.Errorf("could not create monitoring RoleBinding: %w", err)), instance)
 	}
 
-	serviceMonitor := kubernetes.CreateServiceMonitor(
-		instance.Namespace,
-		DeploymentName,
-		monitoringLabels,
-		[]monitoringv1.Endpoint{
-			{
+	if _, err = kubernetes.CreateOrUpdate(ctx, i.Client, &monitoringv1.ServiceMonitor{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      DeploymentName,
+			Namespace: instance.Namespace,
+		},
+	},
+		ensure.ControllerReference[*monitoringv1.ServiceMonitor](instance, i.Client),
+		ensure.Labels[*monitoringv1.ServiceMonitor](maps.Keys(monitoringLabels), monitoringLabels),
+		kubernetes.EnsureServiceMonitorSpec(labels.ForComponent(ComponentName, instance.Name),
+			monitoringv1.Endpoint{
 				Interval: monitoringv1.Duration("30s"),
 				Port:     MetricsPortName,
 				Scheme:   "http",
-			},
-		},
-		labels.ForComponent(ComponentName, instance.Name),
-	)
-
-	if err = controllerutil.SetControllerReference(instance, serviceMonitor, i.Client.Scheme()); err != nil {
-		return i.Failed(fmt.Errorf("could not set controller reference for serviceMonitor: %w", err))
-	}
-
-	if _, err = i.Ensure(ctx, serviceMonitor); err != nil {
-		meta.SetStatusCondition(&instance.Status.Conditions, metav1.Condition{
-			Type:               constants.Ready,
-			Status:             metav1.ConditionFalse,
-			Reason:             constants.Failure,
-			Message:            err.Error(),
-			ObservedGeneration: instance.Generation,
-		})
-		return i.FailedWithStatusUpdate(ctx, fmt.Errorf("could not create serviceMonitor: %w", err), instance)
+			}),
+	); err != nil {
+		return i.Error(ctx, fmt.Errorf("could not create serviceMonitor: %w", err), instance)
 	}
 
 	// monitors & RBAC are not watched - do not need to re-enqueue
