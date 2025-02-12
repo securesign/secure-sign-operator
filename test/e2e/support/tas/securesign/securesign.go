3579ed3aab0e1f34cb01c38fa8a5782fd954fcdb
Jan Bouska <jbouska@redhat.com>
2025-02-12 12:16:12 +0100

    Fix env vars in e2e test

--- test/e2e/support/tas/securesign/securesign.go
+++ test/e2e/support/tas/securesign/securesign.go
@@ -6,22 +6,28 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/securesign/operator/api/v1alpha1"
 	"github.com/securesign/operator/test/e2e/support/condition"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func Verify(ctx context.Context, cli client.Client, namespace string, name string) {
 	Eventually(Get(ctx, cli, namespace, name)).Should(
-		WithTransform(condition.IsReady, BeTrue()))
+		And(
+			Not(BeNil()),
+			WithTransform(condition.IsReady, BeTrue()),
+		))
 }
 
 func Get(ctx context.Context, cli client.Client, ns string, name string) func() *v1alpha1.Securesign {
 	return func() *v1alpha1.Securesign {
 		instance := &v1alpha1.Securesign{}
-		_ = cli.Get(ctx, types.NamespacedName{
+		if e := cli.Get(ctx, types.NamespacedName{
 			Namespace: ns,
 			Name:      name,
-		}, instance)
+		}, instance); errors.IsNotFound(e) {
+			return nil
+		}
 		return instance
 	}
 }
