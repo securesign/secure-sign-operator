01871ff9b6abf1483ae902f4c818e193a3dc0322
Tomas Turek <tturek@redhat.com>
2025-04-11 13:38:20 +0200

    fix: add fulcio's legacy unix socket into no_proxy env vars
    Fixes SECURESIGN-2150

--- internal/controller/common/utils/set_proxy.go
+++ internal/controller/common/utils/set_proxy.go
@@ -1,13 +1,24 @@
 package utils
 
 import (
+	"strings"
+
 	"github.com/operator-framework/operator-lib/proxy"
 	appsv1 "k8s.io/api/apps/v1"
 )
 
 // SetProxyEnvs set the standard environment variables for proxys "HTTP_PROXY", "HTTPS_PROXY", "NO_PROXY"
-func SetProxyEnvs(dep *appsv1.Deployment) {
+func SetProxyEnvs(dep *appsv1.Deployment, noProxy ...string) {
+	envVar := proxy.ReadProxyVarsFromEnv()
+	// add extra values into no_proxy
+	if len(noProxy) > 0 {
+		for i, c := range envVar {
+			if strings.ToLower(c.Name) == "no_proxy" {
+				envVar[i].Value = strings.Join(noProxy, ",") + "," + c.Value
+			}
+		}
+	}
 	for i, container := range dep.Spec.Template.Spec.Containers {
-		dep.Spec.Template.Spec.Containers[i].Env = append(container.Env, proxy.ReadProxyVarsFromEnv()...)
+		dep.Spec.Template.Spec.Containers[i].Env = append(container.Env, envVar...)
 	}
 }
