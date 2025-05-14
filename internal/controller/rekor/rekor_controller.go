e7487053f58f93500a1d663e3775c2c4ec1b2564
Firas Ghanmi <fghanmi@redhat.com>
2025-05-14 09:27:31 +0200

    add alerting

--- internal/controller/rekor/rekor_controller.go
+++ internal/controller/rekor/rekor_controller.go
@@ -30,6 +30,7 @@ import (
 	actions2 "github.com/securesign/operator/internal/controller/rekor/actions"
 	backfillredis "github.com/securesign/operator/internal/controller/rekor/actions/backfillRedis"
 	"github.com/securesign/operator/internal/controller/rekor/actions/monitor"
+	otelcollector "github.com/securesign/operator/internal/controller/rekor/actions/otelCollector"
 	"github.com/securesign/operator/internal/controller/rekor/actions/redis"
 	"github.com/securesign/operator/internal/controller/rekor/actions/server"
 	"github.com/securesign/operator/internal/controller/rekor/actions/ui"
@@ -106,7 +107,7 @@ func (r *rekorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 	target := instance.DeepCopy()
 	actions := []action.Action[*rhtasv1alpha1.Rekor]{
 		transitions.NewToPendingPhaseAction[*rhtasv1alpha1.Rekor](func(rekor *rhtasv1alpha1.Rekor) []string {
-			components := []string{actions2.ServerCondition, actions2.RedisCondition, actions2.SignerCondition}
+			components := []string{actions2.ServerCondition, actions2.RedisCondition, actions2.SignerCondition, actions2.MonitorCondition, actions2.OtelCollectorCondition}
 			if *rekor.Spec.RekorSearchUI.Enabled {
 				components = append(components, actions2.UICondition)
 			}
@@ -149,6 +150,11 @@ func (r *rekorReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl
 		redis.NewInitializeAction(),
 
 		transitions.NewToReadyPhaseAction[*rhtasv1alpha1.Rekor](),
+
+		otelcollector.NewCollectorConfigAction(),
+		otelcollector.NewDeployAction(),
+		otelcollector.NewCreateServiceAction(),
+		otelcollector.NewIngressAction(),
 
 		monitor.NewDeployAction(),
 	}
