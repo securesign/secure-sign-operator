4ed6f6c791b3fa3da400654f3bc097f6924bcc9a
Tommy Dalton <tdalton@redhat.com>
2025-06-27 15:39:15 +0100

    Updating main with new image name

--- cmd/main.go
+++ cmd/main.go
@@ -106,7 +106,7 @@ func main() {
 	utils.RelatedImageFlag("trillian-log-server-image", images.TrillianServer, "The image used for trillian log server.")
 	utils.RelatedImageFlag("trillian-db-image", images.TrillianDb, "The image used for trillian's database.")
 	utils.RelatedImageFlag("trillian-netcat-image", images.TrillianNetcat, "The image used for trillian netcat.")
-	utils.RelatedImageFlag("trillian-create-tree-image", images.TrillianCreateTree, "The image used to create a trillian tree.")
+	utils.RelatedImageFlag("createtree-image", images.TrillianCreateTree, "The image used to create a trillian tree.")
 	utils.RelatedImageFlag("fulcio-server-image", images.FulcioServer, "The image used for the fulcio server.")
 	utils.RelatedImageFlag("rekor-redis-image", images.RekorRedis, "The image used for redis.")
 	utils.RelatedImageFlag("rekor-server-image", images.RekorServer, "The image used for rekor server.")
