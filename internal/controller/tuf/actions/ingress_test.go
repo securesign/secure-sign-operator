f2dc103f9ab5d679efc11fe3d405aec9ca8e2d24
Slavek Kabrda <bkabrda@redhat.com>
2025-07-10 22:27:09 +0200

    fix(SECURESIGN-2058): Fix conditions for creating ingress objects (#1181)
    Ingress objects for Fulcio, Rekor Search UI and TSA will now only
    be created when they should be. Additionally, tests are added for
    all CRDs that have externalAccess config that validate this
    correct behavior.
    
    Refs: SECURESIGN-2058

---
+++ internal/controller/tuf/actions/ingress_test.go
@@ -0,0 +1,59 @@
+package actions
+
+import (
+	"context"
+	"testing"
+
+	. "github.com/onsi/gomega"
+
+	rhtasv1alpha1 "github.com/securesign/operator/api/v1alpha1"
+	"github.com/securesign/operator/internal/constants"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestIngress_CanHandle(t *testing.T) {
+	ctx := context.TODO()
+	tests := []struct {
+		name           string
+		conditions     []metav1.Condition
+		externalAccess bool
+		expected       bool
+	}{
+		{
+			name:           "ingress is enabled and tuf is ready",
+			conditions:     []metav1.Condition{{Type: constants.Ready, Status: metav1.ConditionTrue, Reason: constants.Ready}},
+			externalAccess: true,
+			expected:       true,
+		},
+		{
+			name:           "ingress is enabled and tuf is not ready",
+			conditions:     []metav1.Condition{{Type: constants.Ready, Status: metav1.ConditionFalse, Reason: constants.Pending}},
+			externalAccess: true,
+			expected:       false,
+		},
+		{
+			name:           "ingress is disabled",
+			conditions:     []metav1.Condition{{Type: constants.Ready, Status: metav1.ConditionTrue, Reason: constants.Ready}},
+			externalAccess: false,
+			expected:       false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := NewWithT(t)
+			instance := rhtasv1alpha1.Tuf{
+				Spec: rhtasv1alpha1.TufSpec{
+					ExternalAccess: rhtasv1alpha1.ExternalAccess{
+						Enabled: tt.externalAccess,
+					},
+				},
+				Status: rhtasv1alpha1.TufStatus{
+					Conditions: tt.conditions,
+				},
+			}
+			action := NewIngressAction()
+			g.Expect(tt.expected).To(Equal(action.CanHandle(ctx, &instance)))
+		})
+	}
+}
