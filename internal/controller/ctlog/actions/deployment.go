4d5c59f8560d8ffe5193bb92edb61297333f4c00
Firas Ghanmi <fghanmi@redhat.com>
2025-07-30 16:05:26 +0200

    Set certificate size limits in CTLog

--- internal/controller/ctlog/actions/deployment.go
+++ internal/controller/ctlog/actions/deployment.go
@@ -149,6 +149,7 @@ func (i deployAction) ensureDeployment(instance *rhtasv1alpha1.CTlog, sa string,
 		}
 
 		container.Args = appArgs
+		container.Args = append(container.Args, "--max_cert_chain_size", fmt.Sprintf("%d", *instance.Spec.MaxCertChainSize))
 
 		volumeMount := kubernetes.FindVolumeMountByNameOrCreate(container, volumeName)
 		volumeMount.MountPath = "/ctfe-keys"
