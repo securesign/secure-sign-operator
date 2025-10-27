e01cc2c24d2719eb0e819de87dcc7b79d7ead04b
Petr Pinkas <ppinkas@redhat.com>
2025-10-27 14:21:29 +0100

    Use correct instance name

--- hack/restoreTls.sh
+++ hack/restoreTls.sh
@@ -3,12 +3,20 @@
 # Delete TLS secrets (will be recreated by operator) and restart deployments in correct order.
 # Switch to the namespace you want to delete the TLS secrets from.
 
+# Discover the Securesign instance name
+INSTANCE_NAME=$(oc get Securesign -o jsonpath='{.items[0].metadata.name}')
+
+if [[ -z "$INSTANCE_NAME" ]]; then
+    echo "No Securesign instance found in current namespace"
+    exit 1
+fi
+
 echo "Deleting TLS secrets..."
-oc delete secret securesign-sample-rekor-redis-tls --ignore-not-found=true
-oc delete secret securesign-sample-ctlog-tls --ignore-not-found=true
-oc delete secret securesign-sample-trillian-logserver-tls --ignore-not-found=true
-oc delete secret securesign-sample-trillian-logsigner-tls --ignore-not-found=true
-oc delete secret securesign-sample-trillian-db-tls --ignore-not-found=true
+oc delete secret ${INSTANCE_NAME}-rekor-redis-tls --ignore-not-found=true
+oc delete secret ${INSTANCE_NAME}-ctlog-tls --ignore-not-found=true
+oc delete secret ${INSTANCE_NAME}-trillian-logserver-tls --ignore-not-found=true
+oc delete secret ${INSTANCE_NAME}-trillian-logsigner-tls --ignore-not-found=true
+oc delete secret ${INSTANCE_NAME}-trillian-db-tls --ignore-not-found=true
 
 echo "Restarting Trillian components ..."
 oc rollout restart deployment trillian-db
