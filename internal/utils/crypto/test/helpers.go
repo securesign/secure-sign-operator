4ad09f122107eec628682de662b5c8329a9a9465
JasonPowr <japower@redhat.com>
2025-11-13 11:08:48 +0000

    feat: add checks to validate private and public keys for fips

---
+++ internal/utils/crypto/test/helpers.go
@@ -0,0 +1,144 @@
+package fipsTest
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"testing"
+)
+
+func GenerateRSAPKCS8PEM(t *testing.T, keyBits int) []byte {
+	t.Helper()
+
+	key, err := rsa.GenerateKey(rand.Reader, keyBits)
+	if err != nil {
+		t.Fatalf("failed to generate RSA key: %v", err)
+	}
+
+	der, err := x509.MarshalPKCS8PrivateKey(key)
+	if err != nil {
+		t.Fatalf("failed to marshal PKCS8 key: %v", err)
+	}
+
+	return pem.EncodeToMemory(&pem.Block{
+		Type:  "PRIVATE KEY",
+		Bytes: der,
+	})
+}
+
+func GenerateRSAPKCS1PEM(t *testing.T, keyBits int) []byte {
+	t.Helper()
+
+	key, err := rsa.GenerateKey(rand.Reader, keyBits)
+	if err != nil {
+		t.Fatalf("failed to generate RSA key: %v", err)
+	}
+
+	der := x509.MarshalPKCS1PrivateKey(key)
+
+	return pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: der,
+	})
+}
+
+func GenerateEncryptedRSAPEM(t *testing.T, keyBits int) ([]byte, []byte) {
+	t.Helper()
+
+	key, err := rsa.GenerateKey(rand.Reader, keyBits)
+	if err != nil {
+		t.Fatalf("failed to generate RSA key: %v", err)
+	}
+
+	der := x509.MarshalPKCS1PrivateKey(key)
+	password := []byte("correct-horse-battery-staple")
+
+	block, err := x509.EncryptPEMBlock( //nolint:staticcheck
+		rand.Reader,
+		"RSA PRIVATE KEY",
+		der,
+		password,
+		x509.PEMCipherAES256,
+	)
+	if err != nil {
+		t.Fatalf("failed to encrypt PEM block: %v", err)
+	}
+
+	return pem.EncodeToMemory(block), password
+}
+
+func GenerateECPrivateKeyPEM(t *testing.T, curve elliptic.Curve) []byte {
+	t.Helper()
+
+	key, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		t.Fatalf("failed to generate EC key: %v", err)
+	}
+
+	der, err := x509.MarshalECPrivateKey(key)
+	if err != nil {
+		t.Fatalf("failed to marshal EC private key: %v", err)
+	}
+
+	return pem.EncodeToMemory(&pem.Block{
+		Type:  "EC PRIVATE KEY",
+		Bytes: der,
+	})
+}
+
+func GenerateRSAPKIXPublicKeyPEM(t *testing.T, keyBits int) []byte {
+	t.Helper()
+
+	key, err := rsa.GenerateKey(rand.Reader, keyBits)
+	if err != nil {
+		t.Fatalf("failed to generate RSA key: %v", err)
+	}
+
+	der, err := x509.MarshalPKIXPublicKey(&key.PublicKey)
+	if err != nil {
+		t.Fatalf("failed to marshal PKIX public key: %v", err)
+	}
+
+	return pem.EncodeToMemory(&pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: der,
+	})
+}
+
+func GenerateRSAPKCS1PublicKeyPEM(t *testing.T, keyBits int) []byte {
+	t.Helper()
+
+	key, err := rsa.GenerateKey(rand.Reader, keyBits)
+	if err != nil {
+		t.Fatalf("failed to generate RSA key: %v", err)
+	}
+
+	der := x509.MarshalPKCS1PublicKey(&key.PublicKey)
+
+	return pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: der,
+	})
+}
+
+func GenerateECPublicKeyPEM(t *testing.T, curve elliptic.Curve) []byte {
+	t.Helper()
+
+	key, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		t.Fatalf("failed to generate EC key: %v", err)
+	}
+
+	der, err := x509.MarshalPKIXPublicKey(&key.PublicKey)
+	if err != nil {
+		t.Fatalf("failed to marshal EC public key: %v", err)
+	}
+
+	return pem.EncodeToMemory(&pem.Block{
+		Type:  "PUBLIC KEY",
+		Bytes: der,
+	})
+}
