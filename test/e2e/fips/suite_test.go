db43234a9c797787874d545c80ef80cae07ac05b
JasonPowr <japower@redhat.com>
2025-11-25 15:06:42 +0000

    fips: enable fips checks and add e2e

---
+++ test/e2e/fips/suite_test.go
@@ -0,0 +1,24 @@
+//go:build fips
+
+package fips
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/onsi/gomega/format"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+func TestFipsInstall(t *testing.T) {
+	RegisterFailHandler(Fail)
+	log.SetLogger(GinkgoLogr)
+	SetDefaultEventuallyTimeout(time.Duration(3) * time.Minute)
+	EnforceDefaultTimeoutsWhenUsingContexts()
+	RunSpecs(t, "Fips Install")
+
+	// print whole stack in case of failure
+	format.MaxLength = 0
+}
