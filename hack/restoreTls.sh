b590ddb90a633804a46cc84a93533cfad916aff7
Petr Pinkas <ppinkas@redhat.com>
2025-11-25 09:34:05 +0100

    Restore TLS script simplified

--- hack/restoreTls.sh
+++ hack/restoreTls.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Delete TLS secrets (will be recreated by operator) and restart deployments in correct order.
+# Delete TLS secrets (will be recreated by operator).
 # Switch to the namespace you want to delete the TLS secrets from.
 
 # Discover the Securesign instance name
@@ -17,17 +17,3 @@ oc delete secret ${INSTANCE_NAME}-ctlog-tls --ignore-not-found=true
 oc delete secret ${INSTANCE_NAME}-trillian-logserver-tls --ignore-not-found=true
 oc delete secret ${INSTANCE_NAME}-trillian-logsigner-tls --ignore-not-found=true
 oc delete secret ${INSTANCE_NAME}-trillian-db-tls --ignore-not-found=true
-
-echo "Restarting Trillian components ..."
-oc rollout restart deployment trillian-db
-oc rollout restart deployment trillian-logserver
-oc rollout restart deployment trillian-logsigner
-
-echo "Restarting Redis ..."
-oc rollout restart deployment rekor-redis
-
-echo "Restarting CTlog ..."
-oc rollout restart deployment ctlog
-
-echo "All deployments restarted. New TLS secrets:"
-oc get secrets | grep tls
