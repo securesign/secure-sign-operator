0f774270cfff41fb3907f855b44b52de8badd00f
Petr Pinkas <66317936+petrpinkas@users.noreply.github.com>
2026-02-23 10:06:22 +0100

    SECURESIGN-3873 - Key rotation test temporarily disabled (#1605)

--- test/e2e/key_rotation_test.go
+++ test/e2e/key_rotation_test.go
@@ -47,6 +47,10 @@ import (
 )
 
 var _ = Describe("Key rotation test", Ordered, func() {
+	BeforeAll(func() {
+		Skip("SECURESIGN-3873: key rotation test suite disabled")
+	})
+
 	cli, _ := support.CreateClient()
 	var (
 		targetImageName                             string
