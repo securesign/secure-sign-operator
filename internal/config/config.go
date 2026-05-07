757615210d1071f99b432250f4ebb4b07dfb48d8
Kevin Conner <kev.conner@gmail.com>
2026-05-07 15:33:43 -0700

    fix: SECURESIGN-4401, add retry logic to the platform discovery to handle openshift API Server blackouts

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,10 @@
 package config
 
+import "time"
+
 var (
 	CreateTreeDeadline     int64 = 1200
 	Openshift              bool
 	OpenshiftAPIServerName string
+	APIServerTimeout       time.Duration
 )
