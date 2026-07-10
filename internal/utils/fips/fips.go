5342aedcfe286b4da786f22492437bbd853cfd29
JasonPowr <japower@redhat.com>
2026-07-10 12:26:21 +0100

    feat: restrict client signing algorithms to FIPS-approved set

--- internal/utils/fips/fips.go
+++ internal/utils/fips/fips.go
@@ -8,3 +8,5 @@ import (
 var Enabled = fips140.Enabled
 
 var ErrPasswordRefInFIPS = errors.New("password-protected private keys are not allowed in FIPS mode: remove the password reference and provide an unencrypted key")
+
+const ClientSigningAlgorithms = "ecdsa-sha2-256-nistp256,ecdsa-sha2-384-nistp384,ecdsa-sha2-512-nistp521,rsa-sign-pkcs1-2048-sha256,rsa-sign-pkcs1-3072-sha256,rsa-sign-pkcs1-4096-sha256"
